b53e4549a75064c81769056a5a34bd881da795d9
chiheb rahmouni <chihebrahmouni30@gmail.com>
2024-10-23 12:05:07 +0100

    added redircting from unbox to details

--- src/components/inbox/BoatNameAndPic.tsx
+++ src/components/inbox/BoatNameAndPic.tsx
@@ -1,8 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
+const BoatNameAndPic = ({ details }: any) => {
+
+  const navigate = useNavigate();
+  
+
+  const handleClick = () => {
+navigate(`/boat-details/${details.boatId}`)
+  };
 
 
-const BoatNameAndPic = ({details}: any) => {
   return (
-    <div className="w-full p-2 bg-white mt-5 rounded-10 shadow-hoverShadow flex items-start gap-4">
+    <div className="w-full p-2 bg-white mt-5 rounded-10 shadow-hoverShadow flex items-start gap-4 cursor-pointer" onClick={handleClick}>
       <img
         src={details.boatPic}
         className="w-[130px] h-[90px] object-cover object-center rounded-10"
--- src/containers/InquiryMessages.tsx
+++ src/containers/InquiryMessages.tsx
@@ -1,27 +1,37 @@
-
-const InquiryMessages = ({details}:any) => {
-    return (
+const InquiryMessages = ({ details }: any) => {
+  return (
     <div className="content w-full px-4 mt-5 md:w-[550px] xl:w-[650px]">
-      
-        <div className="flex flex-col space-y-5 lg:space-y-8">
-            {details.messages.map((message:any) => (
-            <div key={message.id} className="flex flex-col space-y-2">
-                <div className="flex items-center space-x-2">
-                <img src={message.sender === "user" ? details.userProfilePic : details.profilePic} className="w-8 h-8 rounded-full bg-gray-300 object-cover object-center" />
-                <div className="flex flex-col">
-                    <span className="font-semibold text-sm">{message.sender ==="user" ? details.userName : details.captainName}</span>
-                    <span className="text-xs text-gray-500">{message.date} {message.time}</span>
-                </div>
-                </div>
-                <div className="bg-gray-100 p-2 rounded-md">
-                <span>{message.message}</span>
-                </div>
+      <div className="flex flex-col space-y-5 lg:space-y-8">
+        {details.messages.map((message: any) => (
+          <div key={message.id} className="flex flex-col space-y-2">
+            <div className="flex items-center gap-2">
+              <img
+                src={
+                  message.sender === "user"
+                    ? details.userProfilePic
+                    : details.profilePic
+                }
+                className="w-8 h-8 rounded-full bg-gray-300 object-cover object-center"
+              />
+              <div className="flex flex-col">
+                <span className="font-semibold text-sm">
+                  {message.sender === "user"
+                    ? details.userName
+                    : details.captainName}
+                </span>
+                <span className="text-xs text-gray-500">
+                  {message.date} {message.time}
+                </span>
+              </div>
             </div>
-            ))}
-        </div>
-  </div>
-    )
+            <div className="bg-gray-100 p-2 rounded-md">
+              <span>{message.message}</span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-}
-
-export default InquiryMessages
+export default InquiryMessages;
