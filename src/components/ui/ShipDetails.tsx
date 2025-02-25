82184d3cde4d87c257a0f8b2c164a2e4fa3b145d
chiheb rahmouni <chihebrahmouni30@gmail.com>
2025-02-25 01:23:19 +0000

    add shipdetails images lasy loading

--- src/components/ui/ShipDetails.tsx
+++ src/components/ui/ShipDetails.tsx
@@ -1,31 +1,31 @@
-import StarRatings from "react-star-ratings";
-import { useNavigate } from "react-router-dom";
-import { useTranslation } from "react-i18next";
+  import StarRatings from "react-star-ratings";
+  import { useNavigate } from "react-router-dom";
+  import { useTranslation } from "react-i18next";
+  import { LazyLoadImage } from "react-lazy-load-image-component";
+  import "react-lazy-load-image-component/src/effects/blur.css";
 
-const url = import.meta.env.VITE_SERVER_URL_LISTING;
-const urlUser = import.meta.env.VITE_SERVER_URL_USERS;
+  const url = import.meta.env.VITE_SERVER_URL_LISTING;
+  const urlUser = import.meta.env.VITE_SERVER_URL_USERS;
 
-const ShipDetails = ({ ship }: any) => {
+  const ShipDetails = ({ ship }: any) => {
+    const navigate = useNavigate();
+    const { t } = useTranslation();
 
-  const navigate = useNavigate();
-  const { t } = useTranslation();
-  
-  const navigateTo = () => {
-    navigate(`/boat-details/${ship.id}`);
-  }
-
-  // console.log(ship);
+    const navigateTo = () => {
+      navigate(`/boat-details/${ship.id}`);
+    };
 
     return (
       <div
         className="relative w-full rounded-[12px] cursor-pointer max-w-[500px] 2xl:max-w-[500px]"
         onClick={navigateTo}
       >
-        <img
+        <LazyLoadImage
           src={`${url}/${ship.Images[0].url}`}
-          // src={ship.Images[0].url}
-          className="w-full h-[200px] object-cover object-center rounded-[12px] lg:h-[190px] 2xl:h-[250px]"
+          width="100%"
+          className="h-[200px] object-cover object-center rounded-[12px] lg:h-[190px] 2xl:h-[250px]"
           alt="boat"
+          effect="blur"
         />
         <div className="texts mt-3 text-writingMainDark px-3">
           <div className="titleRate flex w-full justify-between items-center">
@@ -47,7 +47,8 @@ const ShipDetails = ({ ship }: any) => {
           </p>
           <div className="priceGuests flex items-center justify-between mt-2">
             <p className="text-writingMainDark font-bold text-[16px]">
-              {ship.Prices[0].min_price}-{ship.Prices[0].max_price} {t("rs")} / {t("hour")}
+              {ship.Prices[0].min_price}-{ship.Prices[0].max_price} {t("rs")} /{" "}
+              {t("hour")}
             </p>
             <p className="text-writingGrey text-[13px] lg:text-[13px]">
               {ship.guests} {t("guests")}
@@ -55,24 +56,23 @@ const ShipDetails = ({ ship }: any) => {
           </div>
         </div>
 
-        {ship.user &&
+        {ship.user && (
           <div className="profilePic absolute w-[60px] h-[70px] rounded-10 bg-white top-[-35px] left-[20px] flex items-center justify-center shadow-smallShadow hover:shadow-smallHoverShadow lg:h-[80px] lg:w-[65px]">
-          <img
-            src={
-              ship.user.profilePicture
-                ? `${urlUser}/${ship.user.profilePicture}`
-                : "/anonyme.jpg"
-            }
-            className="w-[47px] h-[47px] object-cover object-center rounded-50 lg:w-[55px] lg:h-[55px]"
-            alt="owner"
-          />
-        </div>}
-       
+            <LazyLoadImage
+              src={
+                ship.user.profilePicture
+                  ? `${urlUser}/${ship.user.profilePicture}`
+                  : "/anonyme.jpg"
+              }
+              className="w-[47px] h-[47px] object-cover object-center rounded-50 lg:w-[55px] lg:h-[55px]"
+              alt="owner"
+              effect="blur"
+            />
+          </div>
+        )}
       </div>
     );
-}
+  };
 
-export default ShipDetails
-
-  
+  export default ShipDetails;
 
