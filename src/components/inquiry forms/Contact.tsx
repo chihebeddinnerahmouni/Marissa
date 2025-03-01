e28402b6ddd59ec7f3a966f388722e3616ea0f01
chiheb rahmouni <chihebrahmouni30@gmail.com>
2025-03-01 12:03:30 +0000

    fix inquiry contact form

--- src/components/inquiry forms/Contact.tsx
+++ src/components/inquiry forms/Contact.tsx
@@ -3,12 +3,14 @@ import { useTranslation } from "react-i18next";
 import { useState, useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { InquiryContext } from "../../Layout/InquiryLayout";
-import PhoneInput from "react-phone-input-2";
-import "react-phone-input-2/lib/style.css";
-import LoadingButton from "../ui/LoadingButton";
 import axios from "axios";
 import Swal from "sweetalert2";
-
+import InputText from "../ui/inputs/InputText";
+import InputEmail from "../ui/inputs/InputEmail";
+import InputTel from "../ui/inputs/InputTel";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import ButtonFunc from "../ui/buttons/Button";
 
 
 const Contact = () => {
@@ -17,14 +19,6 @@ const Contact = () => {
   const navigate = useNavigate();
   const { t } = useTranslation();
   const { setProgress } = useContext(InquiryContext);
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
-  const [isFirstNameMissing, setIsFirstNameMissing] = useState(false);
-  const [isLastNameMissing, setIsLastNameMissing] = useState(false);
-  const [isEmailMissing, setIsEmailMissing] = useState(false);
-  const [isPhoneMissing, setIsPhoneMissing] = useState(false);
   const [loading, setLoading] = useState(false);
   const url = import.meta.env.VITE_SERVER_URL_INQUIRY;
 
@@ -32,30 +26,29 @@ const Contact = () => {
     setProgress((100/6)*6);
   }, []);
 
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      phone: "",
+    },
+    validationSchema: Yup.object({
+      firstName: Yup.string().required(t("name_is_required")),
+      lastName: Yup.string().required(t("surname_is_required")),
+      email: Yup.string().email(t("email_must_be_valid")).required(t("email_is_required")),
+      phone: Yup.string().required(t("phone_is_required")),
+    }),
+    onSubmit: () => {
+      nextHandler();
+    },
+  });
+
 
 
 
 
   const nextHandler = () => { 
-    let isMissing = false;
-    if (firstName === "") {
-      setIsFirstNameMissing(true);
-      isMissing = true;
-    }
-    if (lastName === "") {
-      setIsLastNameMissing(true);
-      isMissing = true;
-    }
-    if (email === "") {
-      setIsEmailMissing(true);
-      isMissing = true;
-    }
-    if (phone === "") {
-      setIsPhoneMissing(true);
-      isMissing = true;
-    }
-    if (isMissing) return;
-
     const inquiry_duration_hours = sessionStorage.getItem("inquiry_duration_hours");
     const inquiry_duration_minutes = sessionStorage.getItem("inquiry_duration_minutes");
     const inquiry_duration_nights = sessionStorage.getItem("inquiry_duration_nights");
@@ -65,8 +58,6 @@ const Contact = () => {
     const inquiry_groupe_childrens = sessionStorage.getItem("inquiry_groupe_childrens");
     const inquiry_groupe_infants = sessionStorage.getItem("inquiry_groupe_infants");
     const inquiry_extra = sessionStorage.getItem("inquiry_extra");
-
-
     // check if there is missing data
     const check = !inquiry_duration_hours || !inquiry_duration_minutes || !inquiry_duration_nights || !inquiry_date || !inquiry_departure || !inquiry_groupe_adultes || !inquiry_groupe_childrens || !inquiry_groupe_infants || !inquiry_extra;
     if (check) return Swal.fire({
@@ -74,10 +65,7 @@ const Contact = () => {
       title: 'ops',
       text: 'missing_data',
     });
-
     setLoading(true);
-
-    // creatint the req.body
     const data = {
       listing_id: boatId,
       tripType: Number(inquiry_duration_nights) > 0 ? "overnight" : "same day",
@@ -96,9 +84,9 @@ const Contact = () => {
       },
       additionalInfo: inquiry_extra,
       contactDetails: {
-        name: firstName + " " + lastName,
-        email: email,
-        phone: phone,
+        name: formik.values.firstName + " " + formik.values.lastName,
+        email: formik.values.email,
+        phone: formik.values.phone,
       },
       status: "pending",
     };
@@ -118,18 +106,16 @@ const Contact = () => {
         navigate(`/inquiry/${boatId}/done`);
       })
       .catch((err) => {
-        console.log(err);
-        if (err.message === "Network Error") {
+        const message = err.message === "Network Error" ? t("network_error") : t("something_went_wrong");
           Swal.fire({
             icon: "error",
-            title: t("network_error"),
-            text: t("please_try_again"),
+            title: t("ops"),
+            text: message,
             customClass: {
               confirmButton: "custom-confirm-button",
             },
           })
           setLoading(false);
-        }
       });
     };
 
@@ -140,85 +126,46 @@ const Contact = () => {
         {t("your_contact_details")}
       </p>
 
-      {/* fields */}
-
-      <div className="names flex w-[320px] gap-2 mt-10">
-        <div className="firstName flex-grow">
-          <input
-            type="text"
-            value={firstName}
-            onChange={(e) => {
-              setFirstName(e.target.value);
-              setIsFirstNameMissing(false);
-            }}
-            placeholder={t("first_name")}
-            className="bg-emptyInput w-full h-8 px-1 rounded-[5px] border-1 border-gray-300 outline-main"
+      <form onSubmit={formik.handleSubmit} className="space-y-5 mt-10">
+        <div className="names flex w-[320px] gap-2">
+          <InputText
+            value={formik.values.firstName}
+            setValue={formik.handleChange("firstName")}
+            label={t("first_name")}
+            error={formik.touched.firstName && Boolean(formik.errors.firstName)}
+            helperText={formik.touched.firstName && formik.errors.firstName}
           />
-          {isFirstNameMissing && (
-            <p className="text-red-500 text-[12px] mt-1">
-              {t("enter_first_name")}
-            </p>
-          )}
-        </div>
-        <div className="firstName flex-grow">
-          <input
-            type="text"
-            value={lastName}
-            onChange={(e) => {
-              setLastName(e.target.value);
-              setIsLastNameMissing(false);
-            }}
-            placeholder={t("last_name")}
-            className="bg-emptyInput w-full h-8 px-1 rounded-[5px] border-1 border-gray-300 outline-main"
+          <InputText
+            value={formik.values.lastName}
+            setValue={formik.handleChange("lastName")}
+            label={t("last_name")}
+            error={formik.touched.lastName && Boolean(formik.errors.lastName)}
+            helperText={formik.touched.lastName && formik.errors.lastName}
           />
-          {isLastNameMissing && (
-            <p className="text-red-500 text-[12px] mt-1">
-              {t("enter_last_name")}
-            </p>
-          )}
         </div>
-      </div>
-
-      <div className="email mt-5 w-[320px]">
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => {
-            setEmail(e.target.value);
-            setIsEmailMissing(false);
-          }}
-          placeholder={t("email")}
-          className="bg-emptyInput w-full h-8 px-1 rounded-[5px] border-1 border-gray-300 outline-main"
+        <InputEmail
+          value={formik.values.email}
+          setValue={formik.handleChange("email")}
+          error={formik.touched.email && Boolean(formik.errors.email)}
+          helperText={formik.touched.email && formik.errors.email}
+          label={t("email")}
         />
-        {isEmailMissing && (
-          <p className="text-red-500 text-[12px] mt-1">{t("enter_email")}</p>
-        )}
-      </div>
-
-      <div className="phone w-[320px] mt-5">
-        <PhoneInput
-          country={"sa"}
-          value={phone}
-          onChange={setPhone}
-          containerClass="flex w-full "
-          inputClass={`flex-grow border border-gray-300 rounded-r-[5px] px-2 focus:border-none focus:outline-main ${
-            isPhoneMissing ? "border-red-400" : "border-gray-300"
-          }`}
-          buttonClass="border border-gray-300 rounded-l-[5px] px-2"
-          dropdownClass="bg-white border border-gray-300 rounded-[5px]"
+        <InputTel
+          value={formik.values.phone}
+          setValue={formik.handleChange("phone")}
+          error={formik.touched.phone && Boolean(formik.errors.phone)}
+          helperText={formik.touched.phone && formik.errors.phone}
+          label={t("phone")}
         />
-        {isPhoneMissing && (
-          <p className="text-red-500 text-[12px] mt-1">{t("enter_phone")}</p>
-        )}
-      </div>
 
-      <button
-        onClick={nextHandler}
-        disabled={loading}
-        className="w-[320px] h-10 bg-mainBlue text-white bg-main rounded-[5px] mt-10"
-      >
-        {loading ? <LoadingButton /> : t("done")}
-      </button>
+        <div className="w-[320px] bg-mainBlue text-white bg-main rounded-[5px] mt-10">
+          <ButtonFunc
+            loading={loading}
+            text={t("done")}
+            type="submit"
+          />
+        </div>
+      </form>
     </div>
   );
 };
--- src/components/inquiry forms/NextButton.tsx
+++ src/components/inquiry forms/NextButton.tsx
@@ -1,6 +1,28 @@
 
+// import React from 'react';
+// import { useTranslation } from 'react-i18next';
+
+// interface NextButtonProps { 
+//     onClick: () => any;
+// }
+
+// const NextButton: React.FC<NextButtonProps> = ({onClick}) => {
+//     const { t } = useTranslation();
+//   return (
+//     <button
+//       onClick={onClick}
+//       className="w-[320px] h-10 bg-mainBlue text-white bg-main rounded-[5px] mt-10"
+//     >
+//       {t("next")}
+//     </button>
+//   );
+// }
+
+// export default NextButton
+
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import ButtonFunc from '@/components/ui/buttons/Button';
 
 interface NextButtonProps { 
     onClick: () => any;
@@ -9,12 +31,12 @@ interface NextButtonProps {
 const NextButton: React.FC<NextButtonProps> = ({onClick}) => {
     const { t } = useTranslation();
   return (
-    <button
-      onClick={onClick}
-      className="w-[320px] h-10 bg-mainBlue text-white bg-main rounded-[5px] mt-10"
-    >
-      {t("next")}
-    </button>
+    <div className="w-[320px] mt-10">
+      <ButtonFunc
+        text={t("next")}
+        onClick={onClick}
+      />
+    </div>
   );
 }
 
