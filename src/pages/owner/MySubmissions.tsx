c5f9b1771da937b475637ea505238e2182c875ab
chiheb rahmouni <chihebrahmouni30@gmail.com>
2025-03-10 16:57:46 +0000

    Refactor MySubmissions component and add axios_error_handler

--- src/pages/owner/MySubmissions.tsx
+++ src/pages/owner/MySubmissions.tsx
@@ -3,25 +3,23 @@ import axios from "axios";
 import { useEffect, useCallback } from "react";
 import LoadingLine from "@/components/ui/LoadingLine";
 import { useNavigate } from "react-router-dom";
-import Swal from "sweetalert2";
 import { useSelector } from "react-redux";
 import { RootState } from "@/redux/store";
-import {useQuery} from "@tanstack/react-query";
-
+import { useQuery } from "@tanstack/react-query";
+import { axios_error_handler } from "@/functions/axios_error_handler";
 
 const MySubmissions = () => {
   const { t } = useTranslation();
   const navigate = useNavigate();
   const user = useSelector((state: RootState) => state.user.user);
 
-  useEffect(() => { 
+  useEffect(() => {
     if (Object.keys(user).length !== 0) {
       if (!user.hasSubmissions) {
-          navigate("/login");
-        }
+        navigate("/login");
+      }
     }
   }, [user]);
-  
 
   const fetchSubmissions = useCallback(async () => {
     const url = import.meta.env.VITE_SERVER_URL_LISTING;
@@ -32,60 +30,47 @@ const MySubmissions = () => {
     });
     return data;
   }, []);
-  
+
   const { data, isLoading, error } = useQuery({
     queryKey: ["mySubmissions"],
     queryFn: fetchSubmissions,
     enabled: user.hasSubmissions,
-  })
+  });
 
-  
-    if (isLoading) {
-      return (
-        <div className="w-full h-screen">
-          <LoadingLine />
-        </div>)
-  }
-  if (error) { 
-    const message =
-      error.message === "Network Error"
-        ? t("network_error")
-        : t("something_went_wrong");
-    Swal.fire({
-      icon: "error",
-      title: t("ops"),
-      text: message,
-      showConfirmButton: false,
-    });
-    return <div className="h-screen"></div>;
-  }
+  useEffect(() => {
+    if (error) axios_error_handler(error, t);
+  }, [error]);
+  if (error) return <div className="h-screen"></div>;
 
-
-
+  if (isLoading) {
     return (
-      <div className="w-full min-h-screen px-4 mt-[100px] md:px-[120px] lg:flex lg:flex-col lg:items-center lg:mt-[130px] 2xl:max-w-[1700px] 2xl:mx-auto">
-        <h1 className="text-[18px] font-semibold text-writingMainDark lg:text-[22px] lg:self-start">
-          {t("all_submissions")}
-        </h1>
-        <p className="text-[14px] font-normal text-writingGrey lg:text-[16px] lg:self-start">
-          {t("view_and_manage_submissions")}
-        </p>
-        <TableComponent submissions={data.submissions} />
+      <div className="w-full h-screen">
+        <LoadingLine />
       </div>
     );
+  }
+
+  return (
+    <div className="w-full min-h-screen px-4 mt-[100px] md:px-[120px] lg:flex lg:flex-col lg:items-center lg:mt-[130px] 2xl:max-w-[1700px] 2xl:mx-auto">
+      <h1 className="text-[18px] font-semibold text-writingMainDark lg:text-[22px] lg:self-start">
+        {t("all_submissions")}
+      </h1>
+      <p className="text-[14px] font-normal text-writingGrey lg:text-[16px] lg:self-start">
+        {t("view_and_manage_submissions")}
+      </p>
+      <TableComponent submissions={data.submissions} />
+    </div>
+  );
 };
 
 export default MySubmissions;
 
-
-const TableComponent = ({ submissions }: { submissions : any[]}) => {
+const TableComponent = ({ submissions }: { submissions: any[] }) => {
   const { t } = useTranslation();
   const navigate = useNavigate();
 
-  console.log(submissions);
-
   const go = useCallback((status: string, id: number) => {
-    switch (status) { 
+    switch (status) {
       case "Approved":
         navigate("/boats-list/title");
         break;
@@ -116,37 +101,32 @@ const TableComponent = ({ submissions }: { submissions : any[]}) => {
             </th>
           </tr>
         </thead>
-        <tbody className="space-y-1">
+        <tbody className="divide-y divide-gray-200">
           {submissions.map((submission: any, index: number) => (
-            <tr key={index} className="bg-white hover:bg-gray-50">
-              <td className="py-3 px-4 border-b text-left text-nowrap">
+            <tr
+              key={index}
+              className="bg-white hover:bg-gray-50 transition duration-200"
+            >
+              <td className="py-3 px-4 text-left whitespace-nowrap">
                 {t(submission.business_type)}
               </td>
-              <td className="py-3 px-4 border-b text-left text-nowrap">
+              <td className="py-3 px-4 text-left whitespace-nowrap">
                 {t(submission.city)}
               </td>
-              <td className="py-3 px-4 border-b text-left text-nowrap">
+              <td className="py-3 px-4 text-left whitespace-nowrap">
                 {t(submission.category)}
               </td>
-              <td
-                className={`py-3 px-4 border-b text-left lg:flex lg:justify-center`}
-              >
+              <td className="py-3 px-4 text-left lg:flex lg:justify-center">
                 <p
-                  className={`p-1 flex items-center justify-center lg:w-[50%] rounded-md text-center text-nowrap ${
+                  className={`p-1 flex items-center justify-center lg:w-[50%] rounded-md text-center whitespace-nowrap cursor-pointer transition duration-200 ${
                     submission.status === "Approved"
-                      ? "bg-green-100 text-[#008767] border-1 border-[#008767] cursor-pointer"
-                      : ""
-                  } ${
-                    submission.status === "Rejected"
-                      ? "bg-red-100 text-[#DF0404] border-1 border-[#DF0404]"
-                      : ""
-                  } 
-                      ${
-                        submission.status === "Document"
-                          ? "bg-orange-100 text-orange-400 border-1 border-orange-400 cursor-pointer"
-                          : ""
-                      }
-                        `}
+                      ? "bg-green-100 text-green-700 border border-green-700"
+                      : submission.status === "Rejected"
+                      ? "bg-red-100 text-red-700 border border-red-700"
+                      : submission.status === "Document"
+                      ? "bg-orange-100 text-orange-700 border border-orange-700"
+                      : "bg-gray-100 text-gray-700 border border-gray-700"
+                  }`}
                   onClick={() => go(submission.status, submission.id)}
                 >
                   {submission.status === "Approved"
