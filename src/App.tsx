4ead756dcc66afe583ce9f43816349b6f1696862
chiheb rahmouni <chihebrahmouni30@gmail.com>
2025-03-11 11:14:21 +0000

    Add useLocation hook to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import NavBar from "./components/ui/NavBar.tsx";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Bounce from "./components/ui/Bounce.tsx";
 import Footer from "./components/ui/Footer.tsx"
 
+
 function App() {
+
+  const location = useLocation();
 
 
   return (
     <>
       <NavBar />
       <Outlet />
-      <Footer />
+      {/* <Footer /> */}
+      {!location.pathname.includes("inbox")&&<Footer />}
       <Bounce />
       <ToastContainer />
     </>
